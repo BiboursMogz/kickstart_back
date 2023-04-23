1049d591bf0e0049e3d3ae8f230a3cb2fc07e7f7
BiboursMogz <antmaugas+github@gmail.com>
2023-04-23 21:45:27 +0200

    Makes DelphiLsp work for one project
    This makes it possible to use Neovim to program in Delphi (sort of) but
    only for a specific project, I need to change it so that Neovim finds
    the *.delphilsp.json file in order to send it the server.

--- lua/custom/plugins/delphi_lsp.lua
+++ lua/custom/plugins/delphi_lsp.lua
@@ -1,27 +1,45 @@
--- local lspconfig = require('lspconfig');
--- local configs = require('lspconfig.configs');
---
--- -- Check if the config is already defined (useful when reloading this file)
--- if not configs.delphi_lsp then
--- 	configs.delphi_lsp = {
--- 		default_config = {
--- 			cmd = {'cmd.exe', '/C', '"C:\\Program Files (x86)\\Embarcadero\\Studio\\21.0\\bin\\DelphiLSP.exe"'},
--- 			filetypes = {'pascal'},
--- 			root_dir = lspconfig.util.root_pattern('*.delphilsp.json'),
--- 			-- root_dir = "C:/Users/Bibours/Documents/codes/delphi/AutoFicheTemps",
--- 			init_options = {
--- 				serverType = "controller",
--- 				agentCount = 2,
--- 				returnDccFlags = true,
--- 				returnHoverModel = true,
--- 				storeProjectSettings = false,
--- 			},
--- 		},
--- 	}
--- end;
---
--- lspconfig.delphi_lsp.setup{
--- 	settingsFile = '"file:///C:\\Users\\Bibours\\Documents\\codes\\delphi\\AutoFicheTemps\\AutoFicheTemps.delphilsp.json"',
--- }
+local lspconfig = require('lspconfig');
+local configs = require('lspconfig.configs');
+
+-- Check if the config is already defined (useful when reloading this file)
+if not configs.delphi_lsp then
+	configs.delphi_lsp = {
+		default_config = {
+			cmd = {'C:\\Program Files (x86)\\Embarcadero\\Studio\\21.0\\bin\\DelphiLSP.exe'},
+			filetypes = {'pascal'},
+			root_dir = lspconfig.util.root_pattern('*.delphilsp.json'),
+			init_options = {
+				serverType = "controller",
+				agentCount = 2,
+				returnDccFlags = true,
+				returnHoverModel = false,
+				storeProjectSettings = false,
+				enableFileWatcher = true,
+			},
+		},
+	}
+end;
+
+lspconfig.delphi_lsp.setup{
+	settings = {
+		settingsFile = 'file:///C:/Users/Bibours/Documents/codes/delphi/AutoFicheTemps/AutoFicheTemps.delphilsp.json',
+	},
+	capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	on_attach = function(_, bufnr)
+	  local nmap = function(keys, func, desc)
+	    if desc then desc = 'LSP: ' .. desc end
+	    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+	  end
+
+	  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+	  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+
+	  -- See `:help K` for why this keymap
+	  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+
+	  -- Lesser used LSP functionality
+	  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+	end,
+}
 
 return {};
