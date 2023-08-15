3422dcfa88c52b26343ce4cf99373b7f6c8e17eb
BiboursMogz <antmaugas+github@gmail.com>
2023-08-15 22:36:29 +0200

    Triggers autoread for GUI applications
    This commit includes code found online that triggers nvim GUIs to
    autoread open buffers
    
    Signed-off-by: BiboursMogz <antmaugas+github@gmail.com>

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -11,3 +11,9 @@ vim.cmd([[
 	let pascal_symbol_operator=1
 ]]);
 
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
+
