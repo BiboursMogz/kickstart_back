482679db38e7046bd07649e913feb1fcfa448b8f
BiboursMogz <antmaugas+github@gmail.com>
2023-08-15 23:43:25 +0200

    Adds TS config outside of init to ensure pascal install.
    This commit adds an helper function in the treesitter config file to
    make sure that the ensure_install table contains 'pascal', if it ever
    breaks in init.lua.
    
    Signed-off-by: BiboursMogz <antmaugas+github@gmail.com>

--- after/treesitter.lua
+++ after/treesitter.lua
@@ -1,4 +1,29 @@
-require'nvim-treesitter.configs'.setup {
+local treesitter_config = require'nvim-treesitter.configs';
+
+local function tblToSet(aTbl)
+  local set = {};
+
+  for _, value in ipairs(aTbl) do
+    set[value] = true;
+  end
+  return set;
+end;
+
+local function my_ensure(old_ensure)
+  if type(old_ensure) == "string" then
+    old_ensure = { old_ensure }
+  end
+  if not tblToSet(old_ensure)["pascal"] then
+    local new_ensure = table.concat(old_ensure, 'pascal');
+    return new_ensure;
+  else
+    return old_ensure;
+  end;
+end;
+
+treesitter_config.setup {
+  -- ensure_installed = treesitter_config.get_ensure_installed_parsers() + 'pascal',
+  ensure_installed = my_ensure(treesitter_config.get_ensure_installed_parsers()),
   highlight = {
     enable = true,
 
@@ -8,4 +33,4 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
-}
+};
